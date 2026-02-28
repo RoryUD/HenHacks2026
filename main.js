a0d885563f7755d85da6d6df30f43907cc965d42
Mizuho Okitani <159543887+okitamisan01@users.noreply.github.com>
2026-02-28 18:55:19 -0500

    solve error

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-//main script
-// Switch from download function to server processing function
+console.log("%c[HenHacks Extension] Loaded!", "color: green; font-size: 20px; font-weight: bold;");
+
+// Wait for the page to fully load
+// window.addEventListener('load') might not fire if the extension loads after the page.
+// Check document.readyState instead.
+
 processAllPages(10).then(() => console.log("Done!"));
-// downloadAllShonenPages(4).then(() => console.log("Done!"));
